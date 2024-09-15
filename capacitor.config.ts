9194de808671c90559d8a9e6328b743a33242fbf
JonaMillaa <166134426+JonaMillaa@users.noreply.github.com>
2024-09-15 05:57:33 -0300

    start app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'appAyudo',
+  appId: 'app.web.ihelpyouapp',
+  appName: 'YoTeAyudo',
   webDir: 'www'
 };
 
